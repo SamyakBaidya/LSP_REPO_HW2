4c4447c40e93c549c83b510c6614949bc937c5f1
Samyak Baidya <69979396+SamyakBaidya@users.noreply.github.com>
2026-03-19 19:45:05 -0400

    Update MemberStrategy.java

--- src/org/howard/edu/lsp/midterm/strategy/MemberStrategy.java
+++ src/org/howard/edu/lsp/midterm/strategy/MemberStrategy.java
@@ -1,9 +1,16 @@
 package org.howard.edu.lsp.midterm.strategy;
 
 /**
- * Strategy for Member customers (10% discount).
+ * Strategy implementation for Member customers.
+ * Applies a 10% discount to the base price.
+ * * @author Samyak Baidya
  */
 public class MemberStrategy implements PricingStrategy {
+    /**
+     * Calculates price for members by applying a 10% discount.
+     * * @param price the original base price
+     * @return the price after a 10% reduction
+     */
     @Override
     public double calculate(double price) {
         return price * 0.90;
