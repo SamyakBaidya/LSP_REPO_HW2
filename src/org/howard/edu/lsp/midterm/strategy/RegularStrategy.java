241818ae84eedc55f51a3f0e1bc0eaee476689e9
Samyak Baidya <69979396+SamyakBaidya@users.noreply.github.com>
2026-03-19 19:44:50 -0400

    Update RegularStrategy.java

--- src/org/howard/edu/lsp/midterm/strategy/RegularStrategy.java
+++ src/org/howard/edu/lsp/midterm/strategy/RegularStrategy.java
@@ -1,9 +1,16 @@
 package org.howard.edu.lsp.midterm.strategy;
 
 /**
- * Strategy for Regular customers (no discount).
+ * Strategy implementation for Regular customers.
+ * Provides no discount (1.0 x price).
+ * * @author Samyak Baidya
  */
 public class RegularStrategy implements PricingStrategy {
+    /**
+     * Returns the original price as no discount is applied for regular customers.
+     * * @param price the original base price
+     * @return the same price as the input
+     */
     @Override
     public double calculate(double price) {
         return price;
